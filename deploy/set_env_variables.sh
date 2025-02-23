1b4177139314ac8046850e072a9449bd41a550d6
kothiba <t.nagano1981@gmail.com>
2025-02-23 13:24:30 +0900

    mod3

--- deploy/set_env_variables.sh
+++ deploy/set_env_variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo DB_HOST=$DB_HOST >> laravel/.env &&
-echo DB_DATABASE=$DB_DATABASE >> laravel/.env &&
-echo DB_USERNAME=$DB_USERNAME >> laravel/.env &&
-echo DB_PASSWORD=$DB_PASSWORD >> laravel/.env &&
-# echo APP_NAME=$APP_NAME >> laravel/.env &&
-echo APP_KEY=$APP_KEY >> laravel/.env &&
-echo APP_ENV=$APP_ENV >> laravel/.env &&
-# echo APP_DEBUG=$APP_DEBUG >> laravel/.env &&
+echo DB_HOST=$DB_HOST >> .env &&
+echo DB_DATABASE=$DB_DATABASE >> .env &&
+echo DB_USERNAME=$DB_USERNAME >> .env &&
+echo DB_PASSWORD=$DB_PASSWORD >> .env &&
+# echo APP_NAME=$APP_NAME >> .env &&
+echo APP_KEY=$APP_KEY >> .env &&
+echo APP_ENV=$APP_ENV >> .env &&
+# echo APP_DEBUG=$APP_DEBUG >> .env &&
 echo 'Laravel env variables configured'
